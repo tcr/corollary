6f405c1cab16795486844fe8739010109b33a960
Paul Collier <paul@paulcollier.ca>
2017-05-23 19:24:27 -0700

    Fix let and where; DoStatement; Assignment

--- parser-haskell/src/ast.rs
+++ parser-haskell/src/ast.rs
@@ -3,10 +3,11 @@ pub enum Expr {
     Number(isize),
     Op(Box<Expr>, String, Box<Expr>),
     Ref(Ident),
-    Do(Vec<Statement>, Vec<Statement>),
+    Do(Vec<DoStatement>),
     Parens(Vec<Expr>),
     Case(Box<Expr>, Vec<CaseCond>),
-    Let(Vec<Statement>, Vec<Statement>),
+    /// `let` a = 2; b = 3 `in` ...
+    Let(Vec<Assignment>, Box<Expr>),
     Span(Vec<Expr>),
     Vector(Vec<Expr>),
     Operator(String),
@@ -19,10 +20,15 @@ pub enum Expr {
 }
 
 #[derive(Clone, Debug)]
+pub struct Assignment {
+    pub pats: Vec<Pat>,
+    pub expr: Expr,
+}
+
+#[derive(Clone, Debug)]
 pub enum CaseCond {
     Matching(Vec<Pat>, Vec<(Vec<Expr>, Expr)>),
     Direct(Vec<Pat>, Vec<Expr>),
-    Where,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -45,13 +51,19 @@ pub enum Statement {
     Instance,
 
     Prototype(Ident, Vec<Ty>),
-    Assign(Vec<Pat>, Expr),
+    Assign(Box<Assignment>, Where),
     GuardAssign,
-    // Expression, where clause
-    Expression(Expr, Vec<Statement>),
 
     // TODO remove this
     Dummy,
+}
+
+pub type Where = Vec<Statement>;
+
+#[derive(Clone, Debug)]
+pub enum DoStatement {
+    Let(Vec<Assignment>),
+    Expression(Box<Expr>),
 }
 
 #[derive(Clone, Debug)]
@@ -63,7 +75,6 @@ pub struct Module {
 #[derive(Clone, Debug)]
 pub enum Ty {
     Span(Vec<Ty>),
-    Where(Box<Ty>, Box<Ty>),
     Pair(Box<Ty>, Box<Ty>),
     Not(Box<Ty>),
     Ref(Ident),
