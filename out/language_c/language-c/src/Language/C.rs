160c5c20918e7c32dacb9c0cebbd9cf52c71e093
Tim Ryan <trim@fb.com>
2017-05-25 16:31:39 -0400

    Adds a scanning file.

---
+++ out/language_c/language-c/src/Language/C.rs
@@ -0,0 +1,23 @@
+pub mod Language_C {
+    use haskell_support::*;
+    pub fn parseCFile(cpp: cpp, tmp_dir_opt: Option<FilePath>, args: Vec<String>, input_file: FilePath) -> IO<Either<ParseError, CTranslUnit>> {
+        /* do */ {
+            let input_stream = __op_bind(if(not, (isPreprocessed(input_file)), then, {
+                    let cpp_args = (rawCppArgs(args, input_file))({
+                            cppTmpDir: tmp_dir_opt
+                        });
+                }, in, runPreprocessor, cpp, cpp_args), handleCppError(else, readInputStream, input_file));
+            return(parseC(input_stream, (initPos(input_file))));
+
+        }
+    }
+
+    pub fn parseCFilePre(file: FilePath) -> IO<Either<ParseError, CTranslUnit>> {
+        /* do */ {
+            let input_stream = readInputStream(file);
+            return(parseC(input_stream, (initPos(file))))
+        }
+    }
+
+}
+
