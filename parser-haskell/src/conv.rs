bf369a352c71f0ca77e4f734d883c70653162e24
Tim Ryan <trim@fb.com>
2017-05-28 10:43:27 -0400

    Fixes up expr_to_pat

--- parser-haskell/src/conv.rs
+++ parser-haskell/src/conv.rs
@@ -2,28 +2,33 @@ use ast::*;
 
 pub fn expr_to_pat(expr: &Expr) -> Pat {
     match *expr {
-        Expr::Dummy => Pat::Dummy,
-
-        //Expr::Op(Box<Expr>, String, Box<Expr>),
         Expr::Operator(ref s) => Pat::Operator(s.to_string()),
         Expr::Ref(ref id) => Pat::Ref(id.clone()),
 
-        //TODO Parens is wrong:
-        Expr::Parens(ref inner) => Pat::Span(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>()),
-        Expr::Span(ref inner) => Pat::Span(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>()),
-        Expr::Vector(ref inner) => Pat::Brackets(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>()),
+        //TODO Parens is wrong? Don't make a span?
+        Expr::Parens(ref inner) => {
+            Pat::Span(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>())
+        },
+        Expr::Span(ref inner) => {
+            Pat::Span(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>())
+        }
+        Expr::Vector(ref inner) => {
+            Pat::Brackets(inner.iter().map(|x| expr_to_pat(x)).collect::<Vec<_>>())
+        }
 
         Expr::Record(ref inner) => Pat::Record(vec![]), // TODO
         Expr::Number(num) => Pat::Num(num),
         Expr::Str(ref s) => Pat::Str(s.clone()),
         Expr::Char(ref s) => Pat::Char(s.clone()),
 
-        //Expr::Case(Box<Expr>, Vec<CaseCond>),
-        //Expr::Let(Vec<Item>, Vec<Item>),
-        //Expr::Do(Vec<Item>, Vec<Item>),
+        Expr::Dummy => Pat::Dummy,
+
+        Expr::Op(..) |
+        Expr::Case(..) |
+        Expr::Let(..) |
+        Expr::Do(..) |
         Expr::Lambda(..) |
-        Expr::Error |
-        _ => {
+        Expr::Error => {
             panic!("Invalid expr to pat conversion: {:?}", expr);
         }
     }
